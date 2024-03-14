365650231b639d656d66289766bfbf8b000ddb4d
MastaKu <59261814+MastaKu@users.noreply.github.com>
2024-03-14 15:54:25 +0200

    Update and rename Project3.java to Project2.java

--- Project2.java
+++ Project2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Project3 {
+public class Project2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите строку a:");
